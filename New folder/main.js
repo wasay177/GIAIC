69ea1211f0fa31c8e42fad36af0870848b2652bb
Abdul Wasay <156638715+wasay177@users.noreply.github.com>
2026-08-20 16:33:15 +0500

    New folder

---
+++ New folder/main.js
@@ -0,0 +1,18 @@
+"use strict";
+let myName = "Ali";
+let myAge = 32;
+console.log("my name is" + myName + "my age is" + myAge);
+console.log(`my name is ${myName} and my age is ${myAge}`);
+// addition
+let station1 = 5;
+let station2 = 2;
+let station3 = "10";
+let sum = `${station1 + station2 + station3}`;
+console.log(sum);
+let num1 = 5;
+let num2 = 10;
+let num3 = 5;
+// let random = "10"
+let sum = num1 + num2 + num3;
+// let sum:any = num1 + num2 + random;
+console.log(sum);
---
+++ New folder/main.ts
@@ -0,0 +1,22 @@
+let myName:string = "Ali";
+let myAge:number = 32;
+
+console.log("my name is" + myName + "my age is" + myAge);
+console.log(`my name is ${myName} and my age is ${myAge}`);
+
+// addition
+let station1:number = 5;
+let station2:number = 2;
+let station3 = "10"
+let sum = `${station1 + station2 + station3}`;
+console.log(sum);
+
+
+let num1:number = 5;                     
+let num2:number = 10;
+let num3:number = 5;
+// let random = "10"
+let sum:number = num1 + num2 + num3;
+// let sum:any = num1 + num2 + random;
+console.log(sum);
+
