bc052bf282f3c924591d4587269cbe289a9479b6
Abdul Wasay <156638715+wasay177@users.noreply.github.com>
2026-08-20 16:32:19 +0500

    14-07-2024

---
+++ 14-07-2024/main.ts
@@ -0,0 +1,95 @@
+// for(let i = 0 ;  i < 10; i++) {
+//     console.log("today is sunday")
+// }
+
+// let i : number = 2;
+
+// while (i < 4) {
+//     console.log("Block statement execution no."+ i)
+//     i++; 
+// }
+
+// For Loop
+// let arrayData = [10, 20, 30, 40, 50];
+// let arrayData: any = {
+//     name: "Fahad",
+//     class: "Sunday  9 - 12",
+// };
+
+// for(let a = 0; a < arrayData.length; a++) {
+//     console.log(a)
+// }
+
+// for(let a = 0; a < arrayData.length; a = a + 2) {
+//     console.log(a)
+// }
+
+// // for(let a = b/2; a < arrayData.length; b--) {
+// //     console.log(a)
+// // }
+
+// for (let element in arrayData) {
+//     console.log("element")
+// }
+
+// for (let element1 in arrayData) {
+//     console.log("element1");
+// } 
+
+// for (let element3 in arrayData) {
+//     console.log("element3");
+// }
+
+// // For Of
+// for (let element of arrayData){
+//     console.log(element);
+// }
+// // For of
+
+// let bt = 9
+// while (bt < 10) {
+//     console.log(bt);
+// }
+
+// let step = 0
+// while (step < 10) {
+//     console.log("Abdullah Moving Towards Gate");
+//     console.log(step);
+//     step = step + 1;
+// }
+
+// let steps:  number = 3;
+// do {
+//     console.log("Block statement execution no." + steps);
+// } while (steps < 4);
+
+// let studentToLeave = ["Bilal", "Saad", "Abdullah", "Asad", "Fahad"];
+
+// for (let index = 0; index < studentToLeave.length; index++) {
+//     if (studentToLeave[index] == "Abdullah") {
+//         console.log("Abdullah Run");
+//         break;
+//     } else {
+//         console.log(studentToLeave[index]);
+//         console.log("Bye Bye !");
+//         ;
+//     }
+// }
+
+enum Role{
+    STUDENT_ROLE = 2796,
+    ADMIN_ROLE = 796,
+} 
+
+const ty = 2796; // This is student role id
+
+if (role_id == 796) {
+    // console.log("Provide Access For Student");
+    unsubscribe();
+    yu();
+}
+
+if (roleId == 879) {
+    TestContext()
+    console.log("");
+}
