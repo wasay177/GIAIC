45702465165183debd8691ff038f580abc3be7f3
Abdul Wasay <156638715+wasay177@users.noreply.github.com>
2026-08-21 15:19:36 +0500

    step02_const_let

---
+++ Practice Work For Exams/Panaverse & learn-typescript/step02_const_let/app.ts
@@ -0,0 +1,17 @@
+// step02_const_let
+// use const where variable values do not change
+const a = 5;
+const b : number = 33;
+const c ="best";
+
+// I suggest use let instead of var everywhere,
+// because let has blocked scope
+if (true) {
+    let z = 4;
+    // use z
+}
+else {
+    let z = "string";
+    // use z
+}
+console.log("let: " + z); // Error: z is not defined in this scope 
