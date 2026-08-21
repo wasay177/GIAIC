ba6a4e9ef93342c21d85940070595f11648bc86a
Abdul Wasay <156638715+wasay177@users.noreply.github.com>
2026-08-21 15:16:24 +0500

    step00d_assignability_error

---
+++ Practice Work For Exams/Panaverse & learn-typescript/step00d_assignability_error/main.js
@@ -0,0 +1,4 @@
+// step00d_assignability_error
+var message = "Hello World";
+message = 6;
+console.log(message);
---
+++ Practice Work For Exams/Panaverse & learn-typescript/step00d_assignability_error/main.ts
@@ -0,0 +1,4 @@
+// step00d_assignability_error
+let message = "Hello World";
+message = 6;
+console.log(message);
