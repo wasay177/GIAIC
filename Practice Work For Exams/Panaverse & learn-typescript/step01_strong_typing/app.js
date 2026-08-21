6fe8971c699798bb15beda66d98198afc594694d
Abdul Wasay <156638715+wasay177@users.noreply.github.com>
2026-08-21 15:18:15 +0500

    step01_strong_typing

---
+++ Practice Work For Exams/Panaverse & learn-typescript/step01_strong_typing/app.js
@@ -0,0 +1,13 @@
+"use strict";
+// step01_strong_typing
+// strongly typed syntax
+let a = "Pakistan";
+a = "USA";
+let b = 9;
+let c = true;
+// type inference
+let e = "USA";
+let f = 10.9;
+f = 22;
+let g = false;
+g = true;
