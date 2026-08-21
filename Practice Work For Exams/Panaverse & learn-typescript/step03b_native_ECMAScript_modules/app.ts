0a87831f5401082d983fa6c997f2bf7a3e1e6666
Abdul Wasay <156638715+wasay177@users.noreply.github.com>
2026-08-21 15:29:01 +0500

    step03b_native_ECMAScript_modules

---
+++ Practice Work For Exams/Panaverse & learn-typescript/step03b_native_ECMAScript_modules/app.ts
@@ -0,0 +1,5 @@
+// step03b_native_ECMAScript_modules
+import a from "./first.js";
+import {b, c} from "./second.js";
+
+console.log(a + b + c);
---
+++ Practice Work For Exams/Panaverse & learn-typescript/step03b_native_ECMAScript_modules/second.ts
@@ -0,0 +1,6 @@
+// step03b_native_ECMAScript_modules
+const b = 10;
+
+const c = 2;
+
+export {b, c};
