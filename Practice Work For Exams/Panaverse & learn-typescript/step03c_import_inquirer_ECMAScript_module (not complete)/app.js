8f1c00432db91f25299fd174e6716cd2e8a09e3b
Abdul Wasay <156638715+wasay177@users.noreply.github.com>
2026-08-21 15:32:04 +0500

    step03c_import_inquirer_ECMAScript_module

---
+++ Practice Work For Exams/Panaverse & learn-typescript/step03c_import_inquirer_ECMAScript_module (not complete)/app.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
+let answers = await inquirer_1.default.prompt([{
+        name: "age",
+        type: "number",
+        message: "Enter your Age:"
+    }
+]);
+console.log("Insha Allah, in " + (60 - answers.age) + "years you will be 60 years old.");
---
+++ Practice Work For Exams/Panaverse & learn-typescript/step03c_import_inquirer_ECMAScript_module (not complete)/app.ts
@@ -0,0 +1,10 @@
+// step03c_import_inquirer_ECMAScript_module
+import inquirer from "inquirer";
+
+let answers = await inquirer.prompt([{
+    name: "age",
+    type: "number",
+    message: "Enter your Age:"}
+]);
+
+console.log("Insha Allah, in " + (60 - answers.age) + "years you will be 60 years old.");
