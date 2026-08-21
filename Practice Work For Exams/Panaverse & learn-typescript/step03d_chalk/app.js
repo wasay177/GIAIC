191068f7c0ce3bb21dc55130357bebda84e19e0f
Abdul Wasay <156638715+wasay177@users.noreply.github.com>
2026-08-21 15:35:07 +0500

    step03d_chalk

---
+++ Practice Work For Exams/Panaverse & learn-typescript/step03d_chalk/app.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
+const chalk_1 = __importDefault(require("chalk"));
+let answers = await inquirer_1.default.prompt([{
+        name: "age",
+        type: "number",
+        message: "Enter your Age:"
+    }
+]);
+console.log(chalk_1.default.blue("Insha Allah, in " + (60 - answers.age) + " years you will be 60 years old."));
